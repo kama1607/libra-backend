482964838583f154f73cd08890146c7b24947a4e
kama1607 <kaxa.ru77@icloud.com>
2022-04-25 07:52:23 +0700

    Рефакторинг кода

--- routes/bookRoute.js
+++ routes/bookRoute.js
@@ -9,6 +9,8 @@ router.get("/books", bookController.getBooks)
 
 router.post("/books", bookController.createBook)
 
+router.get("/books/:id", bookController.getBookById)
+
 router.put("/books/:id", bookController.updateBook)
 
 router.delete("/books/:id", bookController.deleteBook)
